f73adead2ad5a7fb966e01d2ecd7583f86b14056
veronicacheung1105 <veronica.cheung1105@gmail.com>
2018-07-13 21:15:54 +0000

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,2 +1,22 @@
 'use-strict';
 //add your code here
+function h1Selector(){
+  return $('h1')
+}
+
+function liInOlSelector(){
+  return $('ol li')
+}
+
+function linkSelector(){
+  return $('#box4.box5 a')
+}
+
+function imageSelector(){
+  return $("img[alt='cat sleeping']")
+}
+
+function checkboxInputSelector(){
+  return $(":checkbox")
+  // $('input[type='checkbox']'s)
+}
